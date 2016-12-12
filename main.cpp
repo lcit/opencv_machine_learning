3f5ddfb9b00195dd9d5f995f5669fd22034a3eb7
lcit <ldo.citraro@gmail.com>
2016-12-12 15:58:31 -0800

    code formatting

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /*	=========================================================================
 	Author: Leonardo Citraro
-	Company: 
+	Company:
 	Filename: main.cpp
 	Last modifed:   11.12.2016 by Leonardo Citraro
 	Description:    Test of the HOG feature
@@ -27,22 +27,23 @@ void display_superimposed(const cv::Mat& A, const cv::Mat& B, const std::string&
 cv::Mat custom_normalization(const cv::Mat& src) {
     double min, max;
     cv::minMaxLoc(src, &min, &max);
-    cv::Mat dst = src*200/(max-min)+128;
-    dst.convertTo(dst,CV_8U);
+    cv::Mat dst = src * 200 / (max - min) + 128;
+    dst.convertTo(dst, CV_8U);
     return dst;
 }
 
-int main(int argc, char* argv[]){
-    
-    std::vector<float> v = {1,6,7,2,2,2,3};
+int main(int argc, char* argv[]) {
+
+    std::vector<float> v = {1, 6, 7, 2, 2, 2, 3};
     HOG::L2norm(v);
-    for(auto vv:v)
+
+    for (auto vv : v)
         std::cout << vv << ",";
 
     // open and display an image
     cv::Mat image = cv::imread(argv[1], CV_8U);
     cv::imshow("original", image);
-    
+
     // Retrieve the HOG from the image
     size_t blocksize = atoi(argv[2]);
     size_t cellsize = atoi(argv[3]);
@@ -50,20 +51,22 @@ int main(int argc, char* argv[]){
     size_t binning = atoi(argv[5]);
     HOG hog(blocksize, cellsize, stride, binning);
     auto hist = hog.convert(image);
-    
+
     // print the resulting histogram
     std::cout << "Histogram size: " << hist.size() << "\n";
-    for(auto h:hist)
+
+    for (auto h : hist)
         std::cout << h << ",";
+
     std::cout << "\n";
 
     // display some usefull images
     display_superimposed(image, hog.get_vector_mask(), "vector_mask");
     display_superimposed(custom_normalization(hog.get_magnitudes()), hog.get_vector_mask(), "magnitude");
     display_superimposed(custom_normalization(hog.get_orientations()), hog.get_vector_mask(), "orientation");
-    
+
     cv::waitKey();
-    
+
     return 0;
 
 }
